d0b940525ccab1ae3e0a1316e79b74973f8ec8d7
Sam Bowman <sambow23@gmail.com>
2024-09-03 16:41:40 -0500

    add security.nix has security n stuff yeah

--- hosts/p5540/configuration.nix
+++ hosts/p5540/configuration.nix
@@ -5,6 +5,7 @@
     # Include the results of the hardware scan.
     ./hardware-configuration.nix
     ../../main/programs.nix
+    ../../main/security.nix
     ./nvidia.nix
   ];
 
---
+++ main/security.nix
@@ -0,0 +1,12 @@
+{ config, pkgs, inputs, ... }:
+
+{
+    services.fprintd = {
+    enable = true;
+    package = pkgs.fprintd-tod;
+    tod = {
+        enable = true;
+        driver = pkgs.libfprint-2-tod1-vfs0090;
+    };
+  };
+}
