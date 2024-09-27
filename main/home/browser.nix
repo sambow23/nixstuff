b9d36b796392b833771abc70d4c48816f817222e
Sam Bowman <sambow23@gmail.com>
2024-09-27 18:05:54 -0500

    chrome: fix lack of gpu acceleration, enable a few questionable flags my nvidia system might suffer from this

--- main/home/browser.nix
+++ main/home/browser.nix
@@ -9,9 +9,13 @@
         "dhdgffkkebhmkfjojejmpbldmpobfkfo" # tampermonkey for drag-and-freeze fix
       ];
     commandLineArgs = [
-        "--use-angle=opengl" # ty novideo for the driver bugs <3
-        "--enable-features=WebContentsForceDark,WebContentsForceDark:inversion_method/cielab_based/image_behavior/none/foreground_lightness_threshold/150/background_lightness_threshold/205"
+#        "--use-angle=opengl" # ty novideo for the driver bugs <3
+        "--enable-features=WebContentsForceDark,WebContentsForceDark:inversion_method/cielab_based/image_behavior/none/foreground_lightness_threshold/150/background_lightness_threshold/205,DrawImmediatelyWhenInteractive,SkiaGraphite,Vulkan"
         "--disable-smooth-scrolling"
+        "--enable-gpu-rasterization"
+        "--enable-zero-copy"
+        "--ignore-gpu-blocklist"
+        ""
       ];
   };
 }
