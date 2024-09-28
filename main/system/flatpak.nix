1542ee57f3112602785e46209a394c0aff540d3e
Sam Bowman <sambow23@gmail.com>
2024-09-28 09:36:33 -0500

    flatpak: add flatpak.nix hell yea brother

---
+++ main/system/flatpak.nix
@@ -0,0 +1,18 @@
+{ config, pkgs, inputs, ... }:
+
+{
+  services.flatpak.enable = true;
+
+  systemd.services.flatpak-repo = {
+    wantedBy = [ "multi-user.target" ];
+    path = [ pkgs.flatpak ];
+    script = ''
+      flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+    '';
+  };
+
+  services.flatpak.packages = [
+    "com.moonlight_stream.Moonlight"
+    "org.prismlauncher.PrismLauncher"
+  ];
+}
