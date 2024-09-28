09da353b62a82725cd8da357f00d7c39ecabfa74
Sam Bowman <sambow23@gmail.com>
2024-09-28 09:08:40 -0500

    mainpc: modularize config

--- hosts/mainpc/configuration.nix
+++ hosts/mainpc/configuration.nix
@@ -3,7 +3,8 @@
 {
   imports = [
     ./hardware-configuration.nix
-    ../../main/programs.nix
+    ../../main/system/programs.nix
+    ../../main/system/network.nix
     ./nvidia.nix
     ./iscsi.nix
   ];
@@ -11,15 +12,11 @@
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
-  virtualisation.waydroid.enable = true;
   virtualisation.docker.enable = true;
 
   networking.hostName = "mainpc";
 
   nix.settings.experimental-features = ["nix-command" "flakes"];
-
-  # Enable networking
-  networking.networkmanager.enable = true;
 
   # Set your time zone.
   time.timeZone = "America/Chicago";
@@ -39,90 +36,18 @@
     LC_TIME = "en_US.UTF-8";
   };
 
-  # Extra Portal Configuration
-  xdg.portal = {
-    enable = true;
-    wlr.enable = true;
-    extraPortals = [
-      pkgs.xdg-desktop-portal-gtk
-    ];
-    configPackages = [
-      pkgs.xdg-desktop-portal-gtk
-    ];
-  };
-
-
-  # Flatpak
-  services.flatpak.enable = true;
-
-  # Steam
-  programs.steam.enable = true;
-
-  environment.sessionVariables = {
-    NIXOS_OZONE_WL = "1";
-    LIBVA_DRIVER_NAME = "iHD";
-  };
-
-  programs.captive-browser.enable = true;
-  programs.captive-browser.interface = "wlp0s20f3";
-
-  # Enable CUPS to print documents.
-  services.printing.enable = true;
-
-  # Enable sound with pipewire.
   hardware.pulseaudio.enable = false;
   services.pipewire = {
     enable = true;
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
-  };
+    };
 
   users.users.cr = {
     isNormalUser = true;
     description = "cr";
     extraGroups = ["networkmanager" "wheel" "docker"];
-  };
-
-  # Allow unfree packages
-  nixpkgs.config.allowUnfree = true;
-
-  networking.resolvconf.dnsExtensionMechanism = false;
-
-  hardware.bluetooth.enable = true; # enables support for Bluetooth
-  hardware.bluetooth.powerOnBoot = true; # powers up the default Bluetooth controller on boot
-  services.blueman.enable = true;
-  hardware.enableAllFirmware = true;
-
-  networking.firewall = {
-    # if packets are still dropped, they will show up in dmesg
-    logReversePathDrops = true;
-    # wireguard trips rpfilter up
-    extraCommands = ''
-      ip46tables -t mangle -I nixos-fw-rpfilter -p udp -m udp --sport 51820 -j RETURN
-      ip46tables -t mangle -I nixos-fw-rpfilter -p udp -m udp --dport 51820 -j RETURN
-    '';
-    extraStopCommands = ''
-      ip46tables -t mangle -D nixos-fw-rpfilter -p udp -m udp --sport 51820 -j RETURN || true
-      ip46tables -t mangle -D nixos-fw-rpfilter -p udp -m udp --dport 51820 -j RETURN || true
-    '';
-  };
-
-  # Security / Polkit
-    systemd = {
-    user.services.polkit-gnome-authentication-agent-1 = {
-      description = "polkit-gnome-authentication-agent-1";
-      wantedBy = [ "graphical-session.target" ];
-      wants = [ "graphical-session.target" ];
-      after = [ "graphical-session.target" ];
-      serviceConfig = {
-          Type = "simple";
-          ExecStart = "${pkgs.polkit_gnome}/libexec/polkit-gnome-authentication-agent-1";
-          Restart = "on-failure";
-          RestartSec = 1;
-          TimeoutStopSec = 10;
-        };
-    };
   };
 
   # Windows Drive
@@ -131,12 +56,10 @@
     fsType = "ntfs";
   };
 
-  security.rtkit.enable = true;
-  security.pam.services.swaylock = {
-    text = ''
-      auth include login
-    '';
-  };
+  hardware.bluetooth.enable = true;
+  hardware.bluetooth.powerOnBoot = true;
+  services.blueman.enable = true;
+  hardware.enableAllFirmware = true;
 
-  system.stateVersion = "23.11";
+  system.stateVersion = "23.11"; # Did you read the comment?
 }
