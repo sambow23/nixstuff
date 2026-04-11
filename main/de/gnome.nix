02a371306853dfe568adc22ddd6bc62af9babb8d
cr <sambow23@gmail.com>
2026-04-11 09:36:51 -0500

    gnome: add extensions and keyring

--- main/de/gnome.nix
+++ main/de/gnome.nix
@@ -6,5 +6,13 @@
   services.gnome.core-apps.enable = true;
   services.gnome.core-developer-tools.enable = false;
   services.gnome.games.enable = false;
+  services.gnome.gnome-keyring.enable = true;
   environment.gnome.excludePackages = with pkgs; [ gnome-tour gnome-user-docs ];
+
+  environment.systemPackages = with pkgs; [
+    gnome-tweaks
+    pkgs.gnomeExtensions.dash-to-panel
+    pkgs.gnomeExtensions.caffeine
+    pkgs.gnomeExtensions.clipboard-indicator
+  ];
 }
