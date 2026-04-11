03260e8df5b61eb16a9433be2af1aba35eb27bca
cr <sambow23@gmail.com>
2026-04-11 09:46:17 -0500

    vscodium: add back extensions

--- main/home/vscodium.nix
+++ main/home/vscodium.nix
@@ -2,5 +2,10 @@
   programs.vscode = {
     enable = true;
     package = pkgs.vscodium;
+    extensions = with pkgs.vscode-marketplace; [
+      jeff-hykin.better-nix-syntax
+      jnoortheen.nix-ide
+      kamadorueda.alejandra
+    ];
   };
 }
