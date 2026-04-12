bdd6d55f46d042e42d47522a3f1e86a29340b62b
cr <sambow23@gmail.com>
2026-04-11 20:33:38 -0500

    main: move secrets to global location didn't really make sense to keep it housed all in p5540 lol

--- hosts/p5540/configuration.nix
+++ hosts/p5540/configuration.nix
@@ -74,7 +74,7 @@
   hardware.enableAllFirmware = true;
 
   # sops
-  sops.defaultSopsFile = ./home/secrets.yaml;
+  sops.defaultSopsFile = ../../main/secrets.yaml;
   sops.defaultSopsFormat = "yaml";
   sops.age.keyFile = "/home/cr/.config/sops/age/keys.txt";
 
